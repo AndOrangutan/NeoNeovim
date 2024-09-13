4cd72054bdb1d6c3c10eafdf757f896d1dfb331a
Von Mueller <vonjonmueller@gmail.com>
2024-09-12 22:56:42 -0700

    Add options

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,11 +2,12 @@ local _M = {}
 
 _M.did_init = false
 _M.init = function()
-  if did_init then
-    return
-  end
+    if did_init then
+        return
+    end
 
-   require("config.autocmds")
+    require("config.options")
+    require("config.autocmds")
 end
 
 return _M
