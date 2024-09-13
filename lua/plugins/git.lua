9918ed05ef05a98fc241a9871d1ee091db47aad2
Von Mueller <vonjonmueller@gmail.com>
2024-09-12 22:59:50 -0700

    Format files

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,53 +1,53 @@
 return {
-  {
-    "sindrets/diffview.nvim",
-    lazy = true,
-    cmd = {
-      "DiffviewOpen",
-      "DiffviewClose",
-      "DiffviewToggleFiles",
-      "DiffviewFocusFiles",
-      "DiffviewRefresh",
+    {
+        "sindrets/diffview.nvim",
+        lazy = true,
+        cmd = {
+            "DiffviewOpen",
+            "DiffviewClose",
+            "DiffviewToggleFiles",
+            "DiffviewFocusFiles",
+            "DiffviewRefresh",
+        },
     },
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = {"BufReadPost", "BufNewFile", "BufWritePre"},
-    opts = {
-      signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
-      },
-      signs_staged = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
-        changedelete = { text = "▎" },
-      }, 
+    {
+        "lewis6991/gitsigns.nvim",
+        event = {"BufReadPost", "BufNewFile", "BufWritePre"},
+        opts = {
+            signs = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "▎" },
+                untracked = { text = "▎" },
+            },
+            signs_staged = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "▎" },
+            }, 
+        },
     },
-  },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "ibhagwan/fzf-lua",
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "ibhagwan/fzf-lua",
+        },
+        lazy = true,
+        opts = {
+            integrations = {
+                diffview = true,
+                fzf_lua = nil,
+                mini_pick = nil,
+            },
+        },
+        keys = {
+            { '<leader>gg', '<cmd>Neogit<cr>', desc = '[g]it [g]ui'},
+        },
     },
-    lazy = true,
-    opts = {
-      integrations = {
-        diffview = true,
-        fzf_lua = nil,
-        mini_pick = nil,
-      },
-    },
-    keys = {
-      { '<leader>gg', '<cmd>Neogit<cr>', desc = '[g]it [g]ui'},
-    },
-  },
 }
--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      winopts = {
-        border = "none"
-      },
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            winopts = {
+                border = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+            },
+        },
+        lazy = true,
+        cmd = {
+            "FzfLua"
+        },
+        keys = {
+            {"<leader>pf", "<cmd>lua require('fzf-lua').files()<cr>", desc = "[p]icker [f]iles" },
+            {"<leader>pg", "<cmd>lua require('fzf-lua').live_grep()<cr>", desc = "[p]icker [g]rep" },
+            {"<leader>pa", "<cmd>lua require('fzf-lua').builtin()<cr>", desc = "[p]icker All" },
+        },
     },
-    lazy = true,
-    cmd = {
-      "FzfLua"
-    },
-    keys = {
-      {"<leader>pf", "<cmd>lua require('fzf-lua').files()<cr>", desc = "[p]icker [f]iles" },
-      {"<leader>pg", "<cmd>lua require('fzf-lua').live_grep()<cr>", desc = "[p]icker [g]rep" },
-      {"<leader>pa", "<cmd>lua require('fzf-lua').builtin()<cr>", desc = "[p]icker All" },
-    },
-  },
 }
