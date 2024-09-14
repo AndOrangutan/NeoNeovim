d9aafc0542522198ed7d89b9431fdf23188b331e
Von Mueller <vonjonmueller@gmail.com>
2024-09-14 01:18:04 -0700

    Add Lazydev lua-language-server extension

---
+++ lua/plugins/ft-lua.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "folke/lazydev.nvim",
+        dependencies = {
+            "justinsgithub/wezterm-types",
+            "Bilal2453/luvit-meta",
+        },
+        ft = "lua",
+        opts = {
+            library = {
+                -- "~/projects/my-awesome-lib",
+                "lazy.nvim",
+                { path = "luvit-meta/library", words = { "vim%.uv" } },
+                { path = "wezterm-types", mods = { "wezterm" } },
+                -- Load the xmake types when opening file named `xmake.lua`
+                -- Needs `LelouchHe/xmake-luals-addon` to be installed
+                --{ path = "xmake-luals-addon/library", files = { "xmake.lua"
+            },
+        },
+    },
+    { "Bilal2453/luvit-meta", lazy = true },
+    -- TODO: Move to completion
+    -- {
+    --     "hrsh7th/nvim-cmp",
+    --     opts = function(_, opts)
+    --         opts.sources = opts.sources or {}
+    --         table.insert(opts.sources, {
+    --             name = "lazydev",
+    --             group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+    --         })
+    --     end,
+    -- },
+}
