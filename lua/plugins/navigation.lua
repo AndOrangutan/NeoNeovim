87ead4164cdf4ca1fc73cf850f9d2077603ea087
Von Mueller <vonjonmueller@gmail.com>
2024-10-05 22:47:22 -0700

    Temp add

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,17 +1,18 @@
 return { { "mrjones2014/smart-splits.nvim",
         -- config = true,
-        -- lazy = false,
+        lazy = false,
         keys = {
-            { '<A-h>', "<cr>lua require('smart-splits').move_cursor_left()<cr>", desc = "Naviagte Left" },
-            { '<A-j>', "<cr>lua require('smart-splits').move_cursor_down()<cr>", desc = "Naviagte Right" },
-            { '<A-k>', "<cr>lua require('smart-splits').move_cursor_up()<cr>", desc = "Naviagte Up" },
-            { '<A-l>', "<cr>lua require('smart-splits').move_cursor_right()<cr>", desc = "Naviagte Down" },
+            { '<M-h>', "<cr>lua vim.notify('Test')<cr>", desc = "Naviagte Left" },
+            -- { '<M-h>', "<cr>lua require('smart-splits').move_cursor_left()<cr>", desc = "Naviagte Left" },
+            { '<M-j>', "<cr>lua require('smart-splits').move_cursor_down()<cr>", desc = "Naviagte Right" },
+            { '<M-k>', "<cr>lua require('smart-splits').move_cursor_up()<cr>", desc = "Naviagte Up" },
+            { '<M-l>', "<cr>lua require('smart-splits').move_cursor_right()<cr>", desc = "Naviagte Down" },
 
 
-            { '<A-H>', "<cr>lua require('smart-splits').resize_left()<cr>", desc = "Swap Buf Left" },
-            { '<A-J>', "<cr>lua require('smart-splits').resize_down()<cr>", desc = "Swap Buf Right" },
-            { '<A-K>', "<cr>lua require('smart-splits').resize_up()<cr>", desc = "Swap Buf Up" },
-            { '<A-L>', "<cr>lua require('smart-splits').resize_right()<cr>", desc = "Swap Buf Down" },
+            { '<M-H>', "<cr>lua require('smart-splits').resize_left()<cr>", desc = "Swap Buf Left" },
+            { '<M-J>', "<cr>lua require('smart-splits').resize_down()<cr>", desc = "Swap Buf Right" },
+            { '<M-K>', "<cr>lua require('smart-splits').resize_up()<cr>", desc = "Swap Buf Up" },
+            { '<M-L>', "<cr>lua require('smart-splits').resize_right()<cr>", desc = "Swap Buf Down" },
         },
     },
 }
